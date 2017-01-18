18140dc7f8a03d9a59c0e6f23ce55d751b7f8109
Emerson Hum <emersonhum@gmail.com>
2017-01-18 11:59:31 -0800

    Resolve conflict in requirements file

--- airbnbscrape/airbnbscrape/start.py
+++ airbnbscrape/airbnbscrape/start.py
@@ -1,20 +1,16 @@
 from flask import Flask
 
-# from spider import BnbspiderSpider
-
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  print ('hello friend')
   cmd = ['scrapy', 'crawl', 'airspider', '-a', 'city=irvine', '-a', 'country=usa', '-o', 'irvineairbnb.csv']
 
   return subprocess.check_output(cmd)
-  # subprocess.check_output(["echo", "Hello World!"])
-  # subprocess.CompletedProcess( args=['python', 'hello.py'], returncode=0 )
-    # with open('seattleairbnb.csv') as items_file:
-    #   return items_file.read()
+  # return 'hello world'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='172.20.0.2', port='5000')
